315255f6d4eac8c904de3dd8c25c9f798c61957b
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-01-30 00:48:17 -0500

    added models infot

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
+Category.hasMany(Products, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: Vote,
+  as: 'ProductTag',
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: Vote,
+  as: 'ProductTag',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
